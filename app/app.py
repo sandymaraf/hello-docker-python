153bd39b5776aaa7a7eb30cd334e0f803ad6ef18
Sandy <sandymaraf11@gmail.com>
2025-08-08 19:54:00 -0300

    Improving security

--- app/app.py
+++ app/app.py
@@ -22,4 +22,4 @@ def db_check():
         return f"DB Error: {e}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,4 +1,4 @@
 def test_sample():
     result = 1 + 1
     expected = 2
-    assert result == expected
+    assert result == expected  # nosec B101
